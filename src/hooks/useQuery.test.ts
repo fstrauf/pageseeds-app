0983b5c286e3a956a216f4ecd65dfe1dcd542a3e
fstrauf <f.strauf@gmail.com>
2026-04-28 06:06:09 +1200

    refactor(tests): import all necessary functions from vitest in useQuery tests

--- src/hooks/useQuery.test.ts
+++ src/hooks/useQuery.test.ts
@@ -1,5 +1,5 @@
 import { renderHook, act, waitFor } from '@testing-library/react'
-import { vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { useQuery, useMutation, invalidateQueries } from './useQuery'
 
 describe('useQuery', () => {
